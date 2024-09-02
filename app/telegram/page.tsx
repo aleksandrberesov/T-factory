90b5ebeb05665cef2172304e29a33c4971c9c6b0
aleksandrberesov <alexandr.beresov@gmail.com>
2024-09-02 10:43:26 -0300

    full screen in telegram

--- app/telegram/page.tsx
+++ app/telegram/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
         return ()=>{
           console.log(ignore);
           ignore = true;
-
+          FullScreen();
         };            
     }
   }, [t_id]);
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <main>
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          text
+          telegram page
       </p>      
       <div>
         <p 
