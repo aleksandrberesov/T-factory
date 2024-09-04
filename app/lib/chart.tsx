e14c2d479d389142efd0ac2274abf13b001656f0
aleksandrberesov <alexandr.beresov@gmail.com>
2024-09-03 22:12:10 -0300

    chart on 3/4 of screen

--- app/lib/chart.tsx
+++ app/lib/chart.tsx
@@ -24,20 +24,16 @@ function ChartView() {
             chart.timeScale().fitContent();
 
             return ()=>{
-                console.log(ignore);
                 ignore = true;
             };            
         }
     }, [chart_id]);
 
-    //
     return (
         <div
             id = {chart_id}
-            className="left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
-            
+            className="left-0 top-0 flex h-full w-full justify-center"
         >
-        
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -16,44 +16,37 @@ const DoBuyclick = () => {
 }
 
 export default function Home() {
-  const t_id = useId(); 
+  //const t_id = useId(); 
 
   useEffect(() => {
     let ignore = false; 
     if (!ignore){
         FullScreen();
-
         return ()=>{
-          console.log(ignore);
           ignore = true;
         };            
     }
-  }, [t_id]);
+  }, []);
 
   return (
     <main  
-      className="h-dvh w-dvh bg-green-500"
-      //className="min-h-screen lg:flex text-lg flex-col"
+      className="h-dvh w-dvh bg-gray-500 flex-col"
     >
-
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           telegram page
       </p>      
-      <div>
-        <p 
-          id = {t_id}
-          className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            chart name
-        </p> 
+      <div
+        className="h-3/4"
+      > 
         <ChartView />
       </div>  
-      <div className ="flex flex-row items-left">
+      <div className ="flex flex-row items-left w-full justify-center">
         <SelectedTab title="Sell" backgroundcolor="green" onclick={DoSellclick} /> 
         <InputTab title="1000"/>
         <SelectedTab title="Buy" backgroundcolor="red" onclick={DoBuyclick}/>  
-        <SelectedTab title="Close" backgroundcolor="blue" onclick={DoBuyclick}/>      
+        <SelectedTab title="Close" backgroundcolor="blue" onclick={DoBuyclick}/> 
       </div>  
-      <div className="flex-row">
+      <div className="flex flex-row items-left w-full justify-center">
         <SelectedTab icon_image="/icons/play.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
         <SelectedTab icon_image="/icons/pause.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
         <InputTab title="1x"/>
@@ -61,6 +54,7 @@ export default function Home() {
         <SelectedTab icon_image="/icons/stop.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
         <SelectedTab icon_image="/icons/settings.svg" backgroundcolor="gray" onclick={DoBuyclick}/>  
       </div>
+      
     </main>
   );
 }
