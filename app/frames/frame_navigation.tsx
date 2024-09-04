8ce824143fb3ede44e28bfb2bb69ca71532de156
aleksandrberesov <alexandr.beresov@gmail.com>
2024-09-04 07:08:54 -0300

    navigation frame

--- app/frames/frame_navigation.tsx
+++ app/frames/frame_navigation.tsx
@@ -5,7 +5,7 @@ function NavigationFrame(){
         <nav className="bg-gray-800">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                    <div className="hidden sm:ml-6 sm:block">
+                    <div >
                         <div className="flex space-x-3">
                             <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Profile</a>
                             <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Trade</a>
