c85bf2ae2e7b7187156734c974643c65263dd641
aleksandrberesov <alexandr.beresov@gmail.com>
2024-09-04 07:36:35 -0300

    trading frame

--- app/frames/frame_trading.tsx
+++ app/frames/frame_trading.tsx
@@ -1,3 +1,5 @@
+
+
 import React, { useEffect, useState, useId } from 'react';
 import SelectedTab from "../lib/button";
 import ChartView from "../lib/chart";
@@ -13,27 +15,28 @@ const DoBuyclick = () => {
 function TradingFrame(){
     
     return (
-        <div>
+        <div
+            className="h-dvh w-dvh bg-gray-500 flex-col"
+        >
             <div
-        className="h-3/4"
-      > 
-        <ChartView />
-      </div>  
-      <div className ="flex flex-row w-full justify-left">
-        <SelectedTab title="Sell" backgroundcolor="green" onclick={DoSellclick}/> 
-        <InputTab title="1000"/>
-        <SelectedTab title="Buy" backgroundcolor="red" onclick={DoBuyclick}/>  
-        <SelectedTab title="Close" backgroundcolor="blue" onclick={DoBuyclick}/> 
-      </div>  
-      <div className="flex flex-row w-full justify-left">
-        <SelectedTab icon_image="/icons/play.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <SelectedTab icon_image="/icons/pause.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <InputTab title="1x"/>
-        <SelectedTab icon_image="/icons/next.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <SelectedTab icon_image="/icons/stop.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <SelectedTab icon_image="/icons/settings.svg" backgroundcolor="gray" onclick={DoBuyclick}/>  
-      </div>
-
+                className="h-1/2"
+            > 
+                <ChartView />
+            </div>  
+            <div className ="flex flex-row w-full justify-left">
+                <SelectedTab title="Sell" backgroundcolor="green" onclick={DoSellclick}/> 
+                <InputTab title="1000"/>
+                <SelectedTab title="Buy" backgroundcolor="red" onclick={DoBuyclick}/>  
+                <SelectedTab title="Close" backgroundcolor="blue" onclick={DoBuyclick}/> 
+            </div>  
+            <div className="flex flex-row w-full justify-left">
+                <SelectedTab icon_image="/icons/play.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
+                <SelectedTab icon_image="/icons/pause.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
+                <InputTab title="1x"/>
+                <SelectedTab icon_image="/icons/next.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
+                <SelectedTab icon_image="/icons/stop.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
+                <SelectedTab icon_image="/icons/settings.svg" backgroundcolor="gray" onclick={DoBuyclick}/>  
+            </div>
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import SelectedTab from "./lib/button";
 import ChartView from "./lib/chart";
 import InputTab from "./lib/input";
 import NavigationFrame from "./frames/frame_navigation";
+import TradingFrame from "./frames/frame_trading";
 
 const DoSellclick = () => {
   alert("button Sell clicked");
@@ -33,26 +34,8 @@ export default function Home() {
       className="h-dvh w-dvh bg-gray-500 flex-col"
     >
       <NavigationFrame/>
-      <div
-        className="h-3/4"
-      > 
-        <ChartView />
-      </div>  
-      <div className ="flex flex-row w-full justify-left">
-        <SelectedTab title="Sell" backgroundcolor="green" onclick={DoSellclick}/> 
-        <InputTab title="1000"/>
-        <SelectedTab title="Buy" backgroundcolor="red" onclick={DoBuyclick}/>  
-        <SelectedTab title="Close" backgroundcolor="blue" onclick={DoBuyclick}/> 
-      </div>  
-      <div className="flex flex-row w-full justify-left">
-        <SelectedTab icon_image="/icons/play.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <SelectedTab icon_image="/icons/pause.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <InputTab title="1x"/>
-        <SelectedTab icon_image="/icons/next.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <SelectedTab icon_image="/icons/stop.svg" backgroundcolor="gray" onclick={DoBuyclick}/> 
-        <SelectedTab icon_image="/icons/settings.svg" backgroundcolor="gray" onclick={DoBuyclick}/>  
-      </div>
-      
+      <TradingFrame/>
+           
     </main>
   );
 }
