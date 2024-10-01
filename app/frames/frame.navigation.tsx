83da82bc294868ff9b57198770677cc06336b76e
aleksandrberesov <alexandr.beresov@gmail.com>
2024-09-30 21:43:11 -0300

    navigation panel align

--- app/frames/frame.navigation.tsx
+++ app/frames/frame.navigation.tsx
@@ -9,43 +9,36 @@ function NavigationFrame(navigationProps: TNavigationProps){
         navigationProps.setLanguage(LanguageIDs[LanguageIDs.findIndex(element => element.id === id)].element);        
     };
 
+    function CreateButton(id: number): React.JSX.Element{
+        return (
+            <div className='flex-col gap-y-2 col-span-2'>
+                <SelectedTab
+                    id={id}
+                    title={navigationProps.getWord(id)} 
+                    style="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white w-full"
+                    onselected={navigationProps.onselected}
+                />
+            </div>    
+        );
+    };
+
     return(
-        <nav className="bg-gray-800">
-            <div className="mx-auto max-w-7xl px-1 sm:px-1 lg:px-1">
-                <div className="flex flex-1 sm:items-stretch sm:justify-left">
-                        <div className="flex space-x-1 ">
-                            <SelectedTab
-                                id={0}
-                                title={navigationProps.getWord(0)} //"Profile"
-                                //style="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                onselected={navigationProps.onselected}
-                            />
-                            <SelectedTab
-                                id={1}
-                                title={navigationProps.getWord(1)} //"Trade"
-                                //style="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                onselected={navigationProps.onselected}
-                            />
-                            <SelectedTab
-                                id={2}
-                                title={navigationProps.getWord(2)} //"Statistic"
-                                //style="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                onselected={navigationProps.onselected}
-                            />
-                        </div>
-                                          
+        <div className="bg-gray-800">
+            <div className="grid grid-cols-7 gap-2 m-2">
+                    {CreateButton(0)}
+                    {CreateButton(1)}
+                    {CreateButton(2)}
+                <div>             
+                    <DropMenu
+                        elements={LanguageIDs}
+                        selected={LanguageIDs.findIndex(element => element.element === navigationProps.lang)}
+                        style="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+                        dropDirection='down'
+                        onselected={ChangeLanguage}
+                    /> 
                 </div>
-                <div className='sm:items-stretch sm:justify-right'> 
-                            <DropMenu
-                                elements={LanguageIDs}
-                                selected={LanguageIDs.findIndex(element => element.element === navigationProps.lang)}
-                                style="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                                dropDirection='down'
-                                onselected={ChangeLanguage}
-                            />    
-                        </div> 
-            </div>
-        </nav>
+            </div>    
+        </div>
     );
 }
 
