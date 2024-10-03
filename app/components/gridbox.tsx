f27c42af6046eb26ebc881ac15208a2df2af8772
aleksandrberesov <alexandr.beresov@gmail.com>
2024-10-03 09:31:54 -0300

    profile frame

--- app/components/gridbox.tsx
+++ app/components/gridbox.tsx
@@ -5,10 +5,14 @@ function GridBox(gridprops: TGridBoxProps) {
     const listItems = gridprops.elements.map((item) =>
         item.element
     );
-    const gridSettings = ['grid', 'grid-flow-row', [' grid-col-',gridprops.columns?.toString].join("") , ' gap-2 m-2'].join(" ");    
+    let gridSettings = ['grid', 'grid-flow-row', [' grid-col-',gridprops.columns?.toString].join("") , ' gap-2 m-2'].join(" ");
+    gridSettings = [].join( "");
+        
     
     return (
-        <div>
+        <div
+            className="bg-slate-700"
+        >
             <p
                 className="text-black"
             >
--- app/frames/frame.profile.tsx
+++ app/frames/frame.profile.tsx
@@ -3,29 +3,40 @@ import { TProfileProps} from "./types"
 import LabelBox from "../components/label"
 import IconTab from '../components/icontab';
 import CardTab from '../components/cardtab';
-import ListBox from '../components/listbox';
 import GridBox from '../components/gridbox';
+import SelectedTab from '../components/button';
 
 function ProfileFrame(profileprops: TProfileProps){
-    let idCounter = 1;
+    let idCard = 1; 
+    let idStar = 1;
+    const onStarClick = ()=>{
+
+    };
  
+    const StarElements = profileprops.stars.map((item) => {
+        return {
+            id: idStar++,
+            element: <SelectedTab icon_image="/icons/star.svg" onclick={onStarClick}/>
+        };
+    });
     const cardElemets = profileprops.cards.map((item) => {
         return {
-            id: idCounter++,
+            id: idCard++,
             element: <CardTab title={item.title} description={item.description}/>
         }
     });
 
     return(
         <div
-            className="h-screen w-screen bg-transparent gap-y-10"
+            className="h-full w-full bg-transparent gap-y-2 m-2 bg-gray-400"
         >
             <div
-                className='grid-flow-row gap-2 m-2 bg-gray-500'    
+                className='grid-flow-row gap-2 m-2 '    
             >
                 <div>
                     <LabelBox title={profileprops.user.first_name+" "+profileprops.user.last_name} />
                 </div>
+                <GridBox rows={1} elements={StarElements}/>
                 <GridBox rows={3} columns={2} elements={cardElemets}/> 
             </div>
         </div>
