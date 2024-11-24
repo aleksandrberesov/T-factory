b0ecec1555f2314650b5d70d4ca6e8109a0a662c
aleksandrberesov <alexandr.beresov@gmail.com>
2024-11-24 11:17:24 -0300

    aws  settings file

--- app/aws/integration.js
+++ app/aws/integration.js
@@ -1,7 +1,15 @@
 import AWS from 'aws-sdk'; 
-import { awsConfig } from '../aws-exports';
+//import { awsConfig } from '.../aws-exports';
 
 async function GetProfile(user_id){
+    console.log('Region:', process.env.REACT_APP_AWS_REGION); 
+    console.log('Access Key:', process.env.REACT_APP_AWS_ACCESS_KEY_ID); 
+    console.log('Secret Access Key:', process.env.REACT_APP_AWS_SECRET_ACCESS_KEY);
+    const awsConfig = {
+        region: process.env.REACT_APP_AWS_REGION, 
+        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID, 
+        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+    };
     console.log(JSON.stringify(awsConfig, null, 2)); 
     AWS.config.update(awsConfig); 
     const dynamoDB = new AWS.DynamoDB.DocumentClient(); 
