bca0fc23ec0b4519ccf7e7d3be200cd2b89b0881
a.beresov <aleksandr.beresov@gmail.com>
2025-02-15 14:59:24 -0300

    instalation command line script

---
+++ install.bat
@@ -0,0 +1,5 @@
+npm install next@latest react@latest react-dom@latest
+npm install dotenv
+npm install @aws-sdk/client-dynamodb
+npm install @telegram-apps/sdk
+npm install tailwindcss@latest postcss@latest autoprefixer@latest
