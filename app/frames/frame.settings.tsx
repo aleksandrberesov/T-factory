8f3919fe9924aceb0bb49ff03c1a65e962cedee2
aleksandrberesov <alexandr.beresov@gmail.com>
2025-02-25 01:05:23 -0300

    add and delete points

--- app/frames/frame.settings.tsx
+++ app/frames/frame.settings.tsx
@@ -21,28 +21,43 @@ function SettingsFrame(settingsprops: TSettingsFrameProps) {
         setEditPattern(updatedPattern);
     };
 
+    const addNewPoint = (type: 'points' | 'pre_points') => {
+        const newPoint: TPatternPoint = { count: 0, expectation: 0, volatility: 0 };
+        const updatedPattern = { ...editPattern, [type]: [...editPattern[type], newPoint] };
+        setEditPattern(updatedPattern);
+    };
+
+    const deletePoint = (type: 'points' | 'pre_points', id: number) => {
+        const updatedPattern = { ...editPattern, [type]: editPattern[type].filter((_, index) => index !== id) };
+        setEditPattern(updatedPattern);
+    };
+
     const Points = editPattern.points.map((item, index) => ({
         id: index,
         element: (
-            <PointInputGroup
-                key={index}
-                id={index}
-                point={item}
-                onChange={onChange}
-            />
+            <div key={index} className="point-item">
+                <PointInputGroup
+                    id={index}
+                    point={item}
+                    onChange={onChange}
+                />
+                <SelectedTab title='Delete' backgroundcolor='red' onclick={() => deletePoint('points', index)} />
+            </div>
         )
     }));
 
     const PrePoints = editPattern.pre_points.map((item, index) => ({
         id: index,
         element: (
-            <PointInputGroup
-                key={index}
-                id={index}
-                point={item}
-                onChange={onChangePrePoints}
-                prefix="pre-"
-            />
+            <div key={index} className="point-item">
+                <PointInputGroup
+                    id={index}
+                    point={item}
+                    onChange={onChangePrePoints}
+                    prefix="pre-"
+                />
+                <SelectedTab title='Delete' backgroundcolor='red' onclick={() => deletePoint('pre_points', index)} />
+            </div>
         )
     }));
 
@@ -52,15 +67,23 @@ function SettingsFrame(settingsprops: TSettingsFrameProps) {
 
     return (
         <div className="h-screen w-screen bg-transparent gap-y-10">
-            <div className='grid-flow-row gap-2 m-2 bg-gray-500'>
-                <h3>Pre-Points</h3>
-                <ListBox elements={PrePoints} />
-                <h3>Points</h3>
-                <ListBox elements={Points} />
-                <SelectedTab title='Save' /*"Back" */ onclick={HandlePointsChange} />
-                <SelectedTab title={settingsprops.getWord(19)} /*"Back" */ onclick={settingsprops.callBack} />
+            <div className='container m-2 bg-gray-500'>
+                <div className='column'>
+                    <h3>Pre-Points</h3>
+                    <ListBox elements={PrePoints} />
+                    <SelectedTab title='Add Pre-Point' backgroundcolor='green' onclick={() => addNewPoint('pre_points')} />
+                </div>
+                <div className='column'>
+                    <h3>Points</h3>
+                    <ListBox elements={Points} />
+                    <SelectedTab title='Add Point' backgroundcolor='green' onclick={() => addNewPoint('points')} />
+                </div>
             </div>
-        </div>
+            <div className='m-2 bg-gray-500'>
+                <SelectedTab title='Save' onclick={HandlePointsChange} />
+                <SelectedTab title={settingsprops.getWord(19)} onclick={settingsprops.callBack} />
+            </div>
+         </div>
     );
 }
 
