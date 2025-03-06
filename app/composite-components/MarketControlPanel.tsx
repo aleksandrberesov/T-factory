c3f864778a2ad023213974aae47aee98e121838e
a.beresov <aleksandr.beresov@gmail.com>
2025-03-06 10:57:12 -0300

    Control panels

--- app/composite-components/MarketControlPanel.tsx
+++ app/composite-components/MarketControlPanel.tsx
@@ -19,7 +19,6 @@ const MarketControlPanel: React.FC<TMarketControlPanelProps> = (tradeprops) => {
             elements={[
                 {
                     element: !tradeprops.market.isActive ? <SelectedTab icon_image="/icons/play.svg" onclick={tradeprops.market.start}/> : <SelectedTab icon_image="/icons/pause.svg" onclick={tradeprops.market.pause}/>,
-                    row: 1, column: 1, columnSpan: 1, rowSpan: 1,    
                 },
                 {
                     element: 
@@ -31,20 +30,16 @@ const MarketControlPanel: React.FC<TMarketControlPanelProps> = (tradeprops) => {
                             onselected={tradeprops.ChangeSpeed}
                             style="rounded-md px-3 py-2 text-sm font-medium"
                         />,
-                    row: 1, column: 2, columnSpan: 1, rowSpan: 1,
                 },
                 {
                     element: <SelectedTab icon_image="/icons/next.svg" onclick={tradeprops.market.step}/>,
-                    row: 1, column: 3, columnSpan: 1, rowSpan: 1,
                 },
                 {
                     element: <SelectedTab icon_image="/icons/stop.svg" onclick={tradeprops.market.stop}/>,
-                    row: 1, column: 4, columnSpan: 1, rowSpan: 1,
                 },
                 {
                     element: <SelectedTab icon_image="/icons/settings.svg" onclick={tradeprops.HideShowSettings}/>,
-                    row: 1, column: 5, columnSpan: 1, rowSpan: 1,
-                },
+               },
             ]}
         />
     );
--- app/composite-components/TradeControlPanel.tsx
+++ app/composite-components/TradeControlPanel.tsx
@@ -13,19 +13,15 @@ const TradeControlPanel: React.FC<TradeControlPanelProps> = (tradeprops) => {
             elements={[
                 {
                     element: <SelectedTab title={tradeprops.getWord(4)} backgroundcolor="green" textcolor='white' onclick={tradeprops.trader.sell}/>,
-                    row: 1, column: 1, columnSpan: 1, rowSpan: 1,
                 },
                 {
                     element: <SelectedTab title={String(tradeprops.trader.balance)} backgroundcolor='white' textcolor='black'/>,
-                    row: 1, column: 2, columnSpan: 1, rowSpan: 1,
                 },
                 {
                     element: <SelectedTab title={tradeprops.getWord(3)} backgroundcolor="red" textcolor='white' onclick={tradeprops.trader.buy}/>,
-                    row: 1, column: 3, columnSpan: 1, rowSpan: 1,
                 },
                 {
                     element: <SelectedTab title={tradeprops.getWord(5)} backgroundcolor="blue" textcolor='white' onclick={tradeprops.trader.close}/>,
-                    row: 1, column: 4, columnSpan: 1, rowSpan: 1,
                 }
             ]}
         />
