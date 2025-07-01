6e1b883f9e605bf3c57dbfa7722e9535b5b6723c
a.beresov-mac <alexandr.beresov@gmail.com>
2025-07-01 06:08:56 -0300

    bash script for mac/linux

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npm install next@latest react@latest react-dom@latest
+npm install dotenv
+npm install @aws-sdk/client-dynamodb
+npm install @telegram-apps/sdk
+npm i @chakra-ui/react @emotion/react
+npx @chakra-ui/cli snippet add
